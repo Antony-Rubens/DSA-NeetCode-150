0ffb3a0954510b3b9f0a780b9e00dc8c81067d6c
Antony-Rubens <145246163+Antony-Rubens@users.noreply.github.com>
2026-06-29 15:08:12 +0530

    Two sum completed

---
+++ Antony/Two_sum.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class solution:
+    def twosum(self, nums: List[int], target: int) -> List[int]:
+        indices = {}
+        for i, n in enumerate(nums):
+            indices[n] = i
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in indices and indices[diff] != i:
+                return [i, indices[diff]]
+        return []
+
+solution = solution()
+testcase = [2, 3, 4, 6]
+target = 7
+print(solution.twosum(testcase, target))
