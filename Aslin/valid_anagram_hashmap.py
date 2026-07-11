cf4a77fcfe3c8024a692bdbdf886951cecb5307b
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-07-11 12:51:46 +0530

    valid anagram hash map

---
+++ Aslin/valid_anagram_hashmap.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isvalidAnagram(self,s:str,t:str)->bool:
+        if len(s)!=len(t):
+            return False
+        
+        countS,countT={},{}
+        
+        for i in range(len(s)):
+            countS[s[i]]=1+countS.get(s[i],0)
+            countT[t[i]]=1+countT.get(t[i],0)
+
+        return countS == countT
+    
+solution = Solution()
+s="racecar"
+t="carrace"
+print(solution.isvalidAnagram(s,t))
