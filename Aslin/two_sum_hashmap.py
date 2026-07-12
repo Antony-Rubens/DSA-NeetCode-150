bb19edae69b36f86221a2358aecb06b2d189090f
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-07-12 22:01:20 +0530

    two sum - hash map

---
+++ Aslin/two_sum_hashmap.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self,nums:list[int],target:int)->list[int]:
+
+        indices={}
+
+        for i, n in enumerate(nums):
+            indices[n]=i
+
+        for i, n in enumerate(nums):
+            diff = target - n
+
+            if diff in indices and indices[diff] != i:
+                return [i,indices[diff]]
+            
+        return []
+    
+solution=Solution()
+nums=[3,4,5,6,8]
+target=7
+print(solution.twoSum(nums,target))
