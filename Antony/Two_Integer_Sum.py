26d52c4c1d3036e48534976df42e9bb61c0b3999
Antony-Rubens <145246163+Antony-Rubens@users.noreply.github.com>
2026-07-13 22:50:34 +0530

    Two Pointer

---
+++ Antony/Two_Integer_Sum.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def twosum(self, numbers: List[int], target: int)-> List[int]:
+        for i in range(len(numbers)):
+            for j in range(i+1, len(numbers)):
+                if numbers[i]+ numbers[j]== target:
+                    return [i+1,j+1]
+        return []
+Solution = Solution()
+testcase = [1, 2, 3, 4, 5, 6]
+print(Solution.twosum(testcase, 9))
