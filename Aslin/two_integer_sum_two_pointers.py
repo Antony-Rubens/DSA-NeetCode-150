a8a15137c61495a9adc833ebe969928c0a013842
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-07-15 23:16:36 +0530

    two integer sum two pinters

---
+++ Aslin/two_integer_sum_two_pointers.py
@@ -0,0 +1,22 @@
+class Solution:
+    def twoSum(self,numbers:list[int],target:int)->list[int]:
+        l,r=0,len(numbers)-1
+
+        while l<r:
+            curNum=numbers[l]+numbers[r]
+
+            if curNum>target:
+                r-=1
+
+            elif curNum<target:
+                l+=1
+
+            else:
+                return[l+1,r+1]
+            
+        return []
+    
+solution=Solution()
+numbers=[1,2,3,4,5,6]
+target=3
+print(solution.twoSum(numbers,target))
