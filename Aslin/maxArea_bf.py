f1bdccc9059d298b1c615b61dbc3fa6f545d9488
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-07-25 20:12:57 +0530

    Max area brute force

---
+++ Aslin/maxArea_bf.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxArea(self,heights:list[int])->int:
+        res=0
+        for i in range(len(heights)):
+            for j in range(i+1,len(heights)):
+                res=max(res,min(heights[i],heights[j]) * (j-i))
+
+
+        return res
+
+solution=Solution()
+heights=[1,7,2,5,4,7,3,6]
+print(solution.maxArea(heights))
