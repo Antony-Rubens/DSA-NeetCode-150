c756be92aea39d32768cd48e461898988550b19f
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-07-27 22:38:13 +0530

    max profit brute force

---
+++ Aslin/maxProfit.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self,prices:list[int])->int:
+        res=0
+        for i in range(len(prices)):
+            buy=prices[i]
+            for j in range(i+1,len(prices)):
+                sell=prices[j]
+                res=max(res,sell-buy)
+        return res
+
+solution=Solution()
+prices=[7,1,5,3,6,4]
+print(solution.maxProfit(prices))
