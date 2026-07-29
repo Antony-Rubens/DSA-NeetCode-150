31352ee5e3da0a8f94caa059fdb66872bdef54d9
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-07-29 23:46:06 +0530

    maxProfit dynamic solution

---
+++ Aslin/maxProfit_Optimal.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self,prices:list[int])->int:
+        maxP=0
+        minBuy=prices[0]
+
+        for sell in prices:
+            maxP=max(maxP,sell-minBuy)
+            minBuy=min(minBuy,sell)
+
+        return maxP
+
+solution=Solution()
+prices=[7,1,5,3,6,4]
+print(solution.maxProfit(prices))
