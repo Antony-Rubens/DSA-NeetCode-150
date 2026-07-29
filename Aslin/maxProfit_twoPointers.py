e343ca5dbf71a53e3878040283a5b54a728b751a
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-07-29 22:40:40 +0530

    max profit two pointers

---
+++ Aslin/maxProfit_twoPointers.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+
+            r += 1
+
+        return maxP
+
+
+solution = Solution()
+prices = [7, 1, 5, 3, 6, 4]
+print(solution.maxProfit(prices))
