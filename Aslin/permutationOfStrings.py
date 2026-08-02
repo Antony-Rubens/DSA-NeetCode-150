3ff6ba69e2112fde9d55ab729f6caf807a7aedcb
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-08-02 23:05:36 +0530

    permutation of string brute force

---
+++ Aslin/permutationOfStrings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def CheckInclusion(self,s1:str,s2:str) -> bool:
+        s1=sorted(s1)
+        for i in range(len(s2)):
+            for j in range(i,len(s2)):
+                subStr = s2[i:j+1]
+                subStr=sorted(subStr)
+                if subStr == s1:
+                    return True
+        return False
+
+solution=Solution()
+s1="abc"
+s2="lecabee"
+print(solution.CheckInclusion(s1,s2))
