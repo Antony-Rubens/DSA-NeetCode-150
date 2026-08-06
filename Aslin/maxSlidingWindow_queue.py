0feeac3b4ebc270cdd64f59c4d904fb703199b7d
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-08-06 23:05:16 +0530

    maximum sliding window deque

---
+++ Aslin/maxSlidingWindow_queue.py
@@ -0,0 +1,25 @@
+from collections import deque
+class Solution:
+    def maxSlidingWindow(self,nums:list[int],k:int)->int:
+        output=[]
+        q=deque()
+        l=r=0
+        while r < len(nums):
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+        if l > q[0]:
+            q.popleft
+
+        if r+1 >= k:
+            output.append(nums[q[0]])
+            l+=1
+        r+=1
+
+        return output
+
+solution=Solution()
+nums=[1,2,1,0,4,2,6]
+k=3
+print(solution.maxSlidingWindow(nums,k))
