97fcc6d286328793a118b164d9d6e957cf462c34
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-08-07 23:29:59 +0530

    valid parantheses stack

---
+++ Aslin/valisParanthese_stack.py
@@ -0,0 +1,20 @@
+class Solution:
+    def validParanthese(self,s:str)->bool:
+        stack=[]
+        CloseToOpen={")":"(","]":"[","}":"{"}
+
+        for c in s:
+            if c in CloseToOpen:
+                if stack and stack[-1]==CloseToOpen[c]:
+                    stack.pop()
+
+                else:
+                    return False
+
+            else:
+                stack.append(c)
+
+        return True if not stack else False
+solution=Solution()
+s="{}"
+print(solution.validParanthese(s))
